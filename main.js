88d4835554e02dada1c39b0e6d2caa60a60e9ad9
Dilmurod Bukharov <55402928+dilmurodb@users.noreply.github.com>
2023-12-02 19:51:59 +0500

    added matrix image to main tag, made it appear when picture logo on top left is clicked

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ function showResume(block) {
     elem1.style.display = 'none'
     let elem3 = document.getElementById("contacts")
     elem3.style.display = 'none'
+    let elem4 = document.getElementById("image")
+    elem4.style.display = 'none'
     let elem2 = document.getElementById("resume")
     elem2.style.display = block
     let uncheck = document.getElementById("toggle1")
@@ -14,6 +16,8 @@ function showContacts(block) {
     elem1.style.display = 'none'
     let elem2 = document.getElementById("resume")
     elem2.style.display = 'none'
+    let elem4 = document.getElementById("image")
+    elem4.style.display = 'none'
     let elem3 = document.getElementById("contacts")
     elem3.style.display = block
     let uncheck = document.getElementById("toggle1")
@@ -25,8 +29,21 @@ function showProjects(block) {
     elem2.style.display = 'none'
     let elem3 = document.getElementById("contacts")
     elem3.style.display = 'none'
+    let elem4 = document.getElementById("image")
+    elem4.style.display = 'none'
     let elem1 = document.getElementById("projects")
     elem1.style.display = block
     let uncheck = document.getElementById("toggle1")
     uncheck.checked = false
+}
+
+function showImage(block) {
+    let elem1 = document.getElementById("projects")
+    elem1.style.display = 'none'
+    let elem2 = document.getElementById("resume")
+    elem2.style.display = 'none'
+    let elem3 = document.getElementById("contacts")
+    elem3.style.display = 'none'
+    let elem4 = document.getElementById("image")
+    elem4.style.display = block
 }
