98e3e2daa8fcb83582f25b9ac1e1bc79663daff7
Dilmurod Bukharov <55402928+dilmurodb@users.noreply.github.com>
2023-12-02 17:36:15 +0500

    added dom element functionality to switch between main tag content by clicking nav elements

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log('Добро пожаловать')
+function showResume(block) {
+    let elem1 = document.getElementById("projects")
+    elem1.style.display = 'none'
+    let elem3 = document.getElementById("contacts")
+    elem3.style.display = 'none'
+    let elem2 = document.getElementById("resume")
+    elem2.style.display = block
+}
+
+function showContacts(block) {
+    let elem1 = document.getElementById("projects")
+    elem1.style.display = 'none'
+    let elem2 = document.getElementById("resume")
+    elem2.style.display = 'none'
+    let elem3 = document.getElementById("contacts")
+    elem3.style.display = block
+}
+
+function showProjects(block) {
+    let elem2 = document.getElementById("resume")
+    elem2.style.display = 'none'
+    let elem3 = document.getElementById("contacts")
+    elem3.style.display = 'none'
+    let elem1 = document.getElementById("projects")
+    elem1.style.display = block
+}
