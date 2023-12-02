89c69fa353de576478f9d1ab47a342d31493403d
Dilmurod Bukharov <55402928+dilmurodb@users.noreply.github.com>
2023-12-02 19:09:32 +0500

    made buterbrod links function properly and display content in main tag according to selection

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ function showResume(block) {
     elem3.style.display = 'none'
     let elem2 = document.getElementById("resume")
     elem2.style.display = block
+    let uncheck = document.getElementById("toggle1")
+    uncheck.checked = false
 }
 
 function showContacts(block) {
@@ -14,6 +16,8 @@ function showContacts(block) {
     elem2.style.display = 'none'
     let elem3 = document.getElementById("contacts")
     elem3.style.display = block
+    let uncheck = document.getElementById("toggle1")
+    uncheck.checked = false
 }
 
 function showProjects(block) {
@@ -23,4 +27,6 @@ function showProjects(block) {
     elem3.style.display = 'none'
     let elem1 = document.getElementById("projects")
     elem1.style.display = block
+    let uncheck = document.getElementById("toggle1")
+    uncheck.checked = false
 }
